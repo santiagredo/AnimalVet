0f4b582e054248c22299d196081dc695159cafdd
Santiago S <santiagoagredo@live.com>
2023-08-04 16:38:57 -0500

    First commit

---
+++ backend/src/dto/test.ts
@@ -0,0 +1,40 @@
+interface Users {
+    id: number
+    name: string
+    email: string
+    address: string
+    phone: number
+    pets: Pets[]
+}
+
+
+interface Pets {
+    petName: string
+    petSpecie: string
+    petRace: string
+}
+
+// Latest cat appointment at 11:00 Am
+// No cat grooming on saturdays
+// Latest dog appointment at 15:00 Pm, no lunch time
+interface Grooming {
+    SmallAnimalGrooming: 60, 
+    BigAnimalGrooming: 90,
+    CatGrooming: 180
+}
+
+// type Specialities = "Echography" | "X-rays" | "Acupuncture";
+
+// No controls on saturdays
+// Test labs between 9:00 Am and 10:30 Am, last appointment being at 10:15 Am
+// Controls and consults starting at 10:30 Am
+type Services = Grooming | "Consult" | "Test labs" | "Control" | "Speciality";
+
+interface Appointments {
+    type: Services
+    // No sundays or holidays
+    date: string
+    // 9:00 Am - 17:00 Pm
+    // Lunch time: 2:00 Pm - 3:00 Pm
+    hour: string
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+    return <p className="bg-red-500">Testing</p>;
+}
+
+export default App;
