29931fad563c911e40cf054fd10793c844085c0d
Map4che <ljuan648l@gmail.com>
2023-08-05 01:58:46 -0500

    Cambio de Logo por nombre

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,12 @@
 import '../styles/Header.css'
+import logo from '../images/logo.jpg'
 
 export function Header(){
     return(
 
         <header className="container-header">
-            <img src="" alt="Logo" className="logo-header" />
+            {/* <img src={logo} alt="Logo" className="logo-header" /> */}
+            <p className="nombre">Dra Erika Mayte Murcia Agudelo</p>
             <nav className="list-header">
                 <li className="list-header__item">Home</li>
                 <li className="list-header__item">Citas</li>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,7 @@
 import '../styles/Home.css'
+import image1 from '../images/download.jpg';
+import image2 from '../images/razas-de-perros.jpg';
+import image3 from '../images/images.jpg'
 
 export function Home(){
     return(
@@ -8,19 +11,19 @@ export function Home(){
                 <h1 className="title">Separa tu cita</h1>
                 <ul className="list-home">
                     <li className="list-item__home">
-                        <img src="" alt="Gato" className="list-item__image" />
+                        <img src={image1}  alt="Gato" className="list-item__image" />
                         <h2 className="list-item__title">Cita Medica</h2>
                         <p className="list-item__paragraph">El bienestar animal es nuestra prioriedad</p>
                         <button className="list-item__btn">Reservar ahora</button>
                     </li>
                     <li className="list-item__home">
-                        <img src="" alt="Perro" className="list-item__image" />
+                        <img src={image2} alt="Perro" className="list-item__image" />
                         <h2 className="list-item__title">Baño y peluquería</h2>
                         <p className="list-item__paragraph">Dale el mejor look a tu mascota para que luzca brillante</p>
                         <button className="list-item__btn">Reservar ahora</button>
                     </li>
                     <li className="list-item__home">
-                        <img src="" alt="Otro Gato" className="list-item__image" />
+                        <img src={image3}  alt="Otro Gato" className="list-item__image" />
                         <h2 className="list-item__title">Homeopática</h2>
                         <p className="list-item__paragraph">Ciencia avanzada a nuestro alcance</p>
                         <button className="list-item__btn">Reservar ahora</button>
